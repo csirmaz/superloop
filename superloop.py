80cafef1f6633f597071b4b9625ab280fc69e9b8
Elod Csirmaz <csirmazelodpal@gmail.com>
2018-06-09 16:23:13 +0100

    Layer grouping in TensorBoard

--- superloop.py
+++ superloop.py
@@ -1,5 +1,7 @@
 import keras
 import os
+import numpy as np
+
 
 class Builder:
     """Implements building and rebuilding models from cached layers"""
@@ -8,6 +10,7 @@ class Builder:
         self.name = name # Name of the model
         self._shared_layers = {} # layer cache, keyed on _layer_counter
         self._layer_counter = 0
+        self._build_counter = 0
 
     def shared_layer(self, build_function, args, kwargs):
         """Either create a layer shared between all units, or return one from the cache"""
@@ -15,7 +18,7 @@ class Builder:
         if key not in self._shared_layers:
             # Build the layer
             if self.name and ('name' in kwargs): # Generate a name
-                kwargs['name'] = "{}.{}.{}".format(self.name, self._layer_counter, kwargs['name'])
+                kwargs['name'] = "{}/{}.{}".format(self.name, self._layer_counter, kwargs['name'])
             self._shared_layers[key] = build_function(*args, **kwargs)
         
         self._layer_counter += 1
@@ -24,6 +27,7 @@ class Builder:
     def build(self, *args, **kwargs):
         """Build the model"""
         self._layer_counter = 0
+        self._build_counter += 1
         return self._build_impl(*args, **kwargs)
 
 
@@ -35,7 +39,7 @@ class RecurrentUnit(Builder):
         super().__init__(**kwargs)
         # The internal input to use in the next timestep
         ## self.internal_var = keras.layers.Input(tensor=keras.backend.zeros((self.units)), name=self.name + '.ZeroIn')
-        self.zero_in = keras.layers.Input(shape=(self.units,), name=self.name + '.ZeroIn')
+        self.zero_in = keras.layers.Input(shape=(self.units,), name=self.name + '/ZeroIn')
         self.internal_var = self.zero_in
        
     def _build_impl(self, external_input): # TODO Abstract method
@@ -43,7 +47,10 @@ class RecurrentUnit(Builder):
         
         # Vanilla RNN
         
-        x = self.shared_layer(keras.layers.concatenate, ([external_input, self.internal_var],), {'name': 'RecurConcatIn'})
+        x = keras.layers.concatenate(
+            [external_input, self.internal_var],
+            name="{}/ConcatRecur.{}".format(self.name, self._build_counter)
+        )
         # TODO not arbitrary connections!!
         x = self.shared_layer(keras.layers.Dense, (), {'units': self.units, 'name': 'dense'})(x)
         x = self.shared_layer(keras.layers.Activation, ('relu',), {'name': 'relu'})(x)
@@ -61,7 +68,7 @@ class SuperLoopModel(Builder):
         super().__init__(**kwargs)
         # Use this instead of this model's output in the first timestep
         ## self.out = keras.layers.Input(tensor=keras.backend.zeros((self.outputs)), name=self.name + '.ZeroIn')
-        self.zero_in = keras.layers.Input(shape=(self.outputs,), name=self.name + '.ZeroIn')
+        self.zero_in = keras.layers.Input(shape=(self.outputs,), name=self.name + '/ZeroIn')
         self.out = self.zero_in
 
     def _build_impl(self, input): # TODO Abstract method
@@ -77,6 +84,8 @@ class Model(Builder):
     
     def __init__(self, **kwargs):
 
+        super().__init__(**kwargs)
+
         self.config = {
             'recurrent_layers': 5, # number of recurrent layers
             'recurrent_units': 3, # number of recurrent units on each layer
@@ -84,15 +93,13 @@ class Model(Builder):
 
         # Array of builders for the recurrent layers
         self.recurrent_layers = [
-            RecurrentUnit(units=self.config['recurrent_units'], name="Recur{}".format(layerix)) 
+            RecurrentUnit(units=self.config['recurrent_units'], name="{}/Recur{}".format(self.name, layerix)) 
             for layerix in range(self.config['recurrent_layers'])
         ]
         
         # The external system connected via the superloop ("X")
-        self.superloop_model = SuperLoopModel(name="Super")
+        self.superloop_model = SuperLoopModel(name="{}/Super".format(self.name))
         
-        super().__init__(**kwargs)
-
         
     def get_internal_inputs(self):
         inputs = [l.zero_in for l in self.recurrent_layers]
@@ -102,7 +109,10 @@ class Model(Builder):
 
     def _build_impl(self, input): # TODO Abstract method
         """Implements building the model in one timestep"""
-        x = self.shared_layer(keras.layers.concatenate, ([input, self.superloop_model.out],), {'name': 'StepConcatIn'})
+        x = keras.layers.concatenate(
+            [input, self.superloop_model.out],
+            name="{}/ConcatSuper.{}".format(self.name, self._build_counter)
+        )
         for layerix in range(self.config['recurrent_layers']):
 
             x = self.shared_layer(keras.layers.Dense, (), {
@@ -118,25 +128,25 @@ class Model(Builder):
 
 # keras.layers.Input(shape=(10,), name="name1")
 # x = keras.layers.Dense(units=11, name="name2")(x)
-# x = keras.layers.concatenate([x, y], name="name3")
 # keras.layers.TimeDistributed(keras.layers.Dense(12), name="name4")
 
 timesteps = 3
-inputs = [keras.layers.Input(shape=(7,), name="input{}".format(i)) for i in range(timesteps)]
+inputs = [keras.layers.Input(shape=(7,), name="Main/StepInput.{}".format(i)) for i in range(timesteps)]
 mymodelbuilder = Model(name="Main")
+outputs = [None]*timesteps
 
 for timestep in range(timesteps):
-    modelout = mymodelbuilder.build(inputs[timestep])
+    outputs[timestep] = mymodelbuilder.build(inputs[timestep])
 
 inputs.extend(mymodelbuilder.get_internal_inputs())
 
-model = keras.models.Model(inputs=inputs, outputs=modelout)
+model = keras.models.Model(inputs=inputs, outputs=outputs)
 model.compile(loss='categorical_crossentropy',
               optimizer='rmsprop',
               metrics=['accuracy'])
 model.summary()
 
-TensorboardDir = "~/tensorboard_logs/superloop"
+TensorboardDir = "{}/tensorboard_logs/superloop".format(os.environ['HOME'])
 os.system("mkdir -p {}".format(TensorboardDir))
 
 # https://keras.io/callbacks/#tensorboard
@@ -148,11 +158,20 @@ tensorboardcb = keras.callbacks.TensorBoard(
     # write_images=True
 )
 
+batch_size = 32
+
 class DataIterator:
+    
+    def __init__(self):
+        # zero tensors for each input
+        self.ins = [np.zeros((batch_size, t.shape[1])) for t in inputs]
+        self.outs = [np.zeros((batch_size, t.shape[1])) for t in outputs]
     
     def __next__(self):
         """Returns the next batch of data. May run in different processes!"""
-        
+        return (self.ins, self.outs)
+
+my_data = DataIterator()
 
 model.fit_generator(
     generator=my_data,
