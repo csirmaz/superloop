c1ac9081c13c1382b8396ee699084f327c475433
Elod Csirmaz <csirmazelodpal@gmail.com>
2018-06-10 15:13:04 +0100

    Implemented a simple gated unit

--- superloop.py
+++ superloop.py
@@ -1,10 +1,12 @@
+import abc
 import keras
-import os
+from keras import backend as K
 import numpy as np
+import os
 
 
 class Builder:
-    """Implements building and rebuilding models from cached layers"""
+    """Implements building and rebuilding models from cached layers. Abstract class"""
 
     def __init__(self, name=None):
         self.name = name # Name of the model
@@ -30,46 +32,81 @@ class Builder:
         self._build_counter += 1
         return self._build_impl(*args, **kwargs)
 
+    @abc.abstractmethod
+    def _build_impl(self):
+        """Actually build the model"""
+
+
+def CropLayer(start, end, step=1, name=None):
+    """Create a layer that crops out a range from a 1D (+batch) input tensor"""
+    # See https://github.com/keras-team/keras/issues/890
+    return keras.layers.Lambda(lambda x: x[:, start:end:step], output_shape=(int((end-start)/step),), name=name)
+    # Could use K.slice but unsure how to manage batch dimension
+
+def PrintTensor(message):
+    return keras.layers.Lambda(lambda x: K.print_tensor(x, message=message))
 
 class RecurrentUnit(Builder):
-    """(Re)builds a layer of recurrent units"""
+    """Builds a layer of recurrent units"""
     
     def __init__(self, units, **kwargs):
         self.units = units # number of units on the layer
         super().__init__(**kwargs)
         # The internal input to use in the next timestep
-        ## self.internal_var = keras.layers.Input(tensor=keras.backend.zeros((self.units)), name=self.name + '.ZeroIn')
-        self.zero_in = keras.layers.Input(shape=(self.units,), name=self.name + '/ZeroIn')
-        self.internal_var = self.zero_in
-       
-    def _build_impl(self, external_input): # TODO Abstract method
+        # We use an input here as the first dimension (batch size) is not fixed,
+        # so we cannot use a constant tensor.
+        self.first_in = keras.layers.Input(shape=(self.units,), name=self.name + '/FirstIn')
+        self.internal_var = self.first_in
+
+
+    def _build_impl(self, external_input):
         """Implements building the unit itself using shared_layer()"""
         
-        # Vanilla RNN
+        # We implement a very simple gated unit here
+        #
+        # Dense layer
+        # Split the tensor into Input and Control
+        # F = Sigmoid(Control)
+        # Output = F*Internal + (1-F)*ReLU(Input)
         
-        x = keras.layers.concatenate(
-            [external_input, self.internal_var],
-            name="{}/ConcatRecur.{}".format(self.name, self._build_counter)
-        )
-        # TODO not arbitrary connections!!
-        x = self.shared_layer(keras.layers.Dense, (), {'units': self.units, 'name': 'dense'})(x)
-        x = self.shared_layer(keras.layers.Activation, ('relu',), {'name': 'relu'})(x)
+        dense = self.shared_layer(keras.layers.Dense, (), {'units':self.units*2, 'name':'Dense'})(external_input)
 
-        # Split layer: https://github.com/keras-team/keras/issues/890
-        self.internal_var = x
-        return x # external_output
+        f = self.shared_layer(CropLayer, (0, self.units), {'name':'CropF'})(dense) # Probably no need to use shared_layer but the layer will be named properly
+        f = self.shared_layer(keras.layers.Activation, ('hard_sigmoid',), {'name':'Sigm'})(f)
+        # f = PrintTensor("f=sigmoid(ctrl)")(f) # DEBUG
+        
+        ones = self.shared_layer(keras.layers.Lambda, ((lambda x: K.ones_like(x)),), {'name':'Ones'})(f)
+        # Unfortunately, keras.layers.Subtract &c. don't have names, so the graph is unusable
+        ## onesf = self.shared_layer(keras.layers.Subtract, (), {})([ones, f])
+        onesf = self.shared_layer(keras.layers.Lambda, ((lambda x: x[0]-x[1]),), {'name':'Sub'})([ones, f])
+        # onesf = PrintTensor("1-f")(onesf) # DEBUG
+
+        inp = self.shared_layer(CropLayer, (self.units, self.units*2), {'name':'CropIn'})(dense)
+        inp = self.shared_layer(keras.layers.Activation, ('relu',), {'name':'ReLU'})(inp)
+        # inp = PrintTensor("relu(inp)")(inp) # DEBUG
+        ## inp = keras.layers.Multiply()([onesf, inp])
+        inp = self.shared_layer(keras.layers.Lambda, ((lambda x: x[0]*x[1]),), {'name':'MultInp'})([onesf, inp])
+        # inp = PrintTensor("(1-f)*relu(inp)")(inp) # DEBUG
+
+        ## internal = self.shared_layer(keras.layers.Multiply, (), {})([f, self.internal_var])
+        internal = self.shared_layer(keras.layers.Lambda, ((lambda x: x[0]*x[1]),), {'name':'MultInt'})([f, self.internal_var])
+        ## out = self.shared_layer(keras.layers.Add, (), {})([inp, internal])
+        out = self.shared_layer(keras.layers.Lambda, ((lambda x: x[0]+x[1]),), {'name':'Add'})([inp, internal])
+
+        self.internal_var = out
+        return out # external_output
 
 
 class SuperLoopModel(Builder):
-    """(Re)builds the model used in the superloop"""
+    """Builds the model used in the superloop"""
     
     def __init__(self, **kwargs):
         self.outputs = 4 # Number of outputs. We need to know this in advance.
         super().__init__(**kwargs)
         # Use this instead of this model's output in the first timestep
         ## self.out = keras.layers.Input(tensor=keras.backend.zeros((self.outputs)), name=self.name + '.ZeroIn')
-        self.zero_in = keras.layers.Input(shape=(self.outputs,), name=self.name + '/ZeroIn')
-        self.out = self.zero_in
+        self.first_out = keras.layers.Input(shape=(self.outputs,), name=self.name + '/FirstOut')
+        self.out = self.first_out # Always contains the output of the latest build
 
     def _build_impl(self, input): # TODO Abstract method
         """Implements building the unit itself using shared_layer()"""
@@ -78,6 +115,7 @@ class SuperLoopModel(Builder):
         
         self.out = x
         return self.out # output
+
         
 class Model(Builder):
     """Builds the model in one timestep"""
@@ -102,8 +140,9 @@ class Model(Builder):
         
         
     def get_internal_inputs(self):
-        inputs = [l.zero_in for l in self.recurrent_layers]
-        inputs.append(self.superloop_model.zero_in)
+        """Return all the input placeholders automatically generated for the first timestep"""
+        inputs = [l.first_in for l in self.recurrent_layers]
+        inputs.append(self.superloop_model.first_out)
         return inputs
         
 
@@ -111,13 +150,14 @@ class Model(Builder):
         """Implements building the model in one timestep"""
         x = keras.layers.concatenate(
             [input, self.superloop_model.out],
-            name="{}/ConcatSuper.{}".format(self.name, self._build_counter)
+            name="{}/ConcatSuper{}".format(self.name, self._build_counter)
         )
+        
         for layerix in range(self.config['recurrent_layers']):
 
             x = self.shared_layer(keras.layers.Dense, (), {
                 'units': self.config['recurrent_units'], 
-                'name': "main_dense_{}".format(layerix) 
+                'name': "Dense{}".format(layerix) 
             })(x)
 
             x = self.recurrent_layers[layerix].build(x)
@@ -126,12 +166,9 @@ class Model(Builder):
         return x # output
 
 
-# keras.layers.Input(shape=(10,), name="name1")
-# x = keras.layers.Dense(units=11, name="name2")(x)
-# keras.layers.TimeDistributed(keras.layers.Dense(12), name="name4")
 
 timesteps = 3
-inputs = [keras.layers.Input(shape=(7,), name="Main/StepInput.{}".format(i)) for i in range(timesteps)]
+inputs = [keras.layers.Input(shape=(7,), name="Main/StepInput{}".format(i)) for i in range(timesteps)]
 mymodelbuilder = Model(name="Main")
 outputs = [None]*timesteps
 
